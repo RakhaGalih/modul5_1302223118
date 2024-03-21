54d40daf3eac740813315cf06754c14822f1cd33
PRAKTIKAN <PRAKTIKAN@DESKTOP-2G1A4E6>
2024-03-21 10:34:27 +0700

    menambahkan class SimpleDataBase

--- modul5_1302223118/Program.cs
+++ modul5_1302223118/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SimpleDataBase<string> dataBase = new SimpleDataBase<string>();
+        dataBase.AddNewData("13");
+        dataBase.AddNewData("02");
+        dataBase.AddNewData("22");
+        dataBase.PrintAllData();
+    }
+}
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    List<DateTime> inputDates;
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: ");
+
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " Berisi: " + storedData[i] + " yang disimpan pada waktu " + inputDates[i]);
+        }
+    }
+}
